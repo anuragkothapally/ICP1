c91c007513169a97a54fc10ff323dc91ac1a8588
anuragkothapally <97029052+anuragkothapally@users.noreply.github.com>
2024-05-20 22:51:24 -0500

    Create mainq6.py

---
+++ mainq6.py
@@ -0,0 +1,7 @@
+sentence = "I am a teacher and I love to inspire and teach people"
+words = sentence.split()
+print("Words in the sentence:", words)
+unique_words = set(words)
+print("Unique words in the sentence:", unique_words)
+num_unique_words = len(unique_words)
+print("Number of unique words:", num_unique_words)
